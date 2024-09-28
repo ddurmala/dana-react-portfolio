df96178ebfd92867b55a0d51fb149dd755f98c6a
ddurmala <169076397+ddurmala@users.noreply.github.com>
2024-09-28 13:39:40 -0400

    updated resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,22 +1,43 @@
+import fileDownload from 'bootstrap-icons/icons/file-earmark-arrow-down.svg'
+
+import fileText from 'bootstrap-icons/icons/file-text.svg'
+
 function Resume() {
 
     const resumeURL = 'https://drive.google.com/file/d/1igS2bvKwcgxOp1c1gOCDetXXvQMBxn7S/view?usp=sharing'
 
+    const gDocResume = 'https://docs.google.com/document/d/1tjrQz4MHVP33DOwslxzLYuS4C4ctpSBOxIkYzOk1d3I/edit?usp=drive_link';
+
     return (
         <>
-            <section className="text-center p-4 my-5">
-                <h5>Click the link below to download my full resume:</h5>
-                <p className="my-5 resume-link">
+            <section className="text-center p-4 mt-5">
+                <h5 className='mb-5'>Click the link below to download my full resume:</h5>
+                <p className="my-3 resume-link">
 
-                    <div className="mb-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
-                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
-                        </svg>
-                    </div>
+                    <a href={resumeURL} download="DC-Dev-Resume.pdf" >
 
-                    <a href={resumeURL} download="dc-resume-2024.pdf" >Download Resume</a>
+                        <img style={{ height: '2.75rem' }} src={fileDownload} className='mb-3' />
+                        <br />
+
+                        <p>Download Resume</p>
+                    </a>
                 </p>
+            </section>
+
+            <section className="text-center p-4 my-3">
+
+                {/* Google Docs versions of Resume */}
+
+                <h5>
+                    ...for a Google Docs version of my resume, click here :
+                </h5>
+
+                <a href={gDocResume} className='d-flex justify-content-center align-items-center mt-4 resume-link'>
+                    <img style={{ height: '2rem' }} src={fileText} className='mb-0 ' />
+
+                    <p className='mb-0 ms-2 fs-3'>Dana's Resume (Google Docs)</p>
+                </a>
+
             </section>
         </>
     )
